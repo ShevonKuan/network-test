831be8408c8490de9353c0c2971ed5645476eac4
ShevonKuan <331749486@qq.com>
2023-03-08 21:33:40 +0800

    modified:   .gitignore 	modified:   README.md 	modified:   cmd/root.go 	modified:   cmd/speedtest.go 	new file:   cmd/tcping.go

--- cmd/speedtest.go
+++ cmd/speedtest.go
@@ -13,12 +13,7 @@ import (
 var speedtestCmd = &cobra.Command{
 	Use:   "speedtest",
 	Short: "speedtest function",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		threads, _ := cmd.Flags().GetInt("multi-threads")
 		size, _ := cmd.Flags().GetString("size")
